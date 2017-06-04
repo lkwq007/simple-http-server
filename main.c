b30f401ce8df053ac69330b145d8675542569c1c
Death <lkwq007@gmail.com>
2017-06-04 14:44:46 +0800

    Modify comments, add credits, add SIGINT handler

--- main.c
+++ main.c
@@ -1,24 +1,6 @@
-//#include <stdio.h>
-//#include <pthread.h>
-//#include "thpool.h"
-//
-//
-//void task1(){
-//	printf("Thread #%u working on task1\n", (int)pthread_self());
-//}
-//
-//
-//void task2(){
-//	printf("Thread #%u working on task2\n", (int)pthread_self());
-//}
-
-
-//int main(){
-//	
-
-//}
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
@@ -30,7 +12,9 @@
 #include <sys/stat.h>
 #include <sys/sendfile.h>
 #include <sys/select.h>
+// https://github.com/dermesser/libsocket
 #include "libinetsocket.h"
+// https://github.com/Pithikos/C-Thread-Pool
 #include "thpool.h"
 #define MIMETYPE "mime-types.tsv"
 #define HTTP_200 0
@@ -38,12 +22,15 @@
 #define HTTP_500 2
 #define HTTP_GET 0
 #define HTTP_POST 1
+// http convention & template
 char http_status[3][64] = { "200 OK","404 Not Found","500 Internal Server Error" };
 char http_template[256] = "HTTP/1.1 %s\nServer: Lnyan's Simple HTTP Server\nContent-Length: %d\nContent-Type: %s\n\n";
 char verify_template[256] = "<!DOCTYPE html><html><head><title>%s</title></head><body><p>%s</p></body></html>";
+// login&pass for /dopost, it's lab requirement
 char LOGIN[128] = "username";
 char PASS[128] = "password";
 char working_dir[1024] = ".";
+// client info
 struct Client_
 {
 	char *host;
@@ -61,7 +48,7 @@ Client initClient(char *host, char *port, int cfd, int sfd)
 	p->sfd = sfd;
 	return p;
 }
-
+// decode url
 void urldecode2(char *dst, const char *src)
 {
 	char a, b;
@@ -98,7 +85,10 @@ void urldecode2(char *dst, const char *src)
 	}
 	*dst++ = '\0';
 }
-
+// reference: https://github.com/Menghongli/C-Web-Server/blob/master/get-mime-type.c
+// I changed strtok to strtok_r for multithread
+// This function is not efficient
+// TODO: use ext-type array for searching
 char * get_mime_type(char *name) {
 	char *ext = strrchr(name, '.');
 	char delimiters[] = " \n";
@@ -142,30 +132,30 @@ char * get_mime_type(char *name) {
 	}
 	return mime_type;
 }
-
+// judge whether the path is a regular file
 int is_regular_file(const char *path)
 {
 	struct stat path_stat;
 	stat(path, &path_stat);
 	return S_ISREG(path_stat.st_mode);
 }
-
+// server instance for handing stream
 int server(Client client)
 {
-	fd_set input;
 	int ret, length, header_done = 0, bytes_left = 0, buf_offset = 0;
 	int method, get_file;
 	struct stat get_stat;
 	off_t offset = 0L;
 	char buf[8192], header[8192], body[8192], *delimiter, field[128], url[2048] ,durl[2048] ,path[2048]=".";
-	char rep[8192], *rep_type, *match, dir_buf[512];;
+	char rep[8192], *rep_type, *match, dir_buf[512];
 	int rep_stat=0, rep_len=0;
-	struct timeval timeout;
+	//struct timeval timeout;
+	//fd_set input;
+	//timeout.tv_sec = 0;
+	//timeout.tv_usec = 2000;
+	//FD_ZERO(&input);
+	//FD_SET(client->cfd, &input);
 	strcpy(path, working_dir);
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 2000;
-	FD_ZERO(&input);
-	FD_SET(client->cfd, &input);
 	while (1)
 	{
 		//ret = select(client->cfd + 1, &input, NULL, NULL, &timeout);
@@ -190,7 +180,7 @@ int server(Client client)
 			length = 1;
 		}
 		ret = recv(client->cfd, buf + buf_offset, length*sizeof(char), 0);
-		printf("%u: recv %d bytes from %s:%s\n", (int)pthread_self(),ret, client->host, client->port);
+		printf("id#%u: recv %d bytes from %s:%s\n", (int)pthread_self(),ret, client->host, client->port);
 		if (ret < 0)
 		{
 			printf("Connectiont Error with %s:%s\n", client->host, client->port);
@@ -232,11 +222,10 @@ int server(Client client)
 			}
 			if (header_done)
 			{
-				char *temp, c,*token;
-				//match = malloc(8192 * sizeof(char));
-				//strcpy(match, header);
+				char *temp, *token;
 				match = header;
 				token = match;
+				method = -1;
 				while ((temp = strtok_r(match, "\n", &match))!=NULL)
 				{
 					// using temp may cause seg fault, i cannot figure out why it happened
@@ -318,9 +307,8 @@ int server(Client client)
 				}
 				case HTTP_POST:
 				{
-					//int login, pass;
 					char login[128], pass[128];
-					//rep_type = strdup("text/html");
+					//rep_type = strdup("text/html");//strdup may cause seg fault
 					rep_type = malloc(128 * sizeof(char));
 					strcpy(rep_type, "text/html");
 					if (!strcmp(url, "/dopost"))
@@ -400,6 +388,7 @@ int server(Client client)
 						else
 						{
 							rep_stat = HTTP_200;
+							offset = 0L;
 							fstat(get_file, &get_stat);
 							rep_len = get_stat.st_size;
 							sprintf(header, http_template, http_status[rep_stat], rep_len, rep_type);
@@ -417,7 +406,7 @@ int server(Client client)
 						strcpy(rep_type, "text/html");
 						rep_len = 0;
 						rep[0] = '\0';
-						if (dir = opendir(path))
+						if ((dir = opendir(path)))
 						{
 							strcat(rep, "<!DOCTYPE html><html><head><title>Directory</title></head><body>");
 							if (url[strlen(url) - 1] != '/')
@@ -464,9 +453,21 @@ int server(Client client)
 	}
 	shutdown_inet_stream_socket(client->cfd, LIBSOCKET_BOTH);
 	destroy_inet_socket(client->cfd);
-	printf("end\n");
+	printf("id#%u end\n", (int)pthread_self());
 	free(client);
 	return 0;
+}
+
+// socket fd & thread pool
+int sfd;
+threadpool thpool;
+// exit signal
+volatile sig_atomic_t running_flag = 1;
+void sig_handler(int sig)
+{
+	running_flag = 0; // set flag
+	destroy_inet_socket(sfd);
+	return;
 }
 
 int main(int argc,char *argv[])
@@ -475,15 +476,18 @@ int main(int argc,char *argv[])
 	int current_dir = 1;
 	DIR *dir;
 	char address[32] = "0.0.0.0", port[32] = "8080";
-	int sfd = create_inet_server_socket(address, port, LIBSOCKET_TCP, LIBSOCKET_IPv4, 0);
+	signal(SIGINT, sig_handler);
+	// listening
+	sfd = create_inet_server_socket(address, port, LIBSOCKET_TCP, LIBSOCKET_IPv4, 0);
 	if (sfd < 0)
 	{
 		printf("Error, cannot create server socket");
 		exit(1);
 	}
 	printf("Listening %s at port %s..\n", address, port);
-	threadpool thpool = thpool_init(16);
+	thpool = thpool_init(16);
 	printf("Thead pool has started.\n");
+	// checking working directory
 	if (argc > 1)
 	{
 		if (argv[1][0] == '/')
@@ -495,7 +499,7 @@ int main(int argc,char *argv[])
 			strcpy(cwd, "./");
 			strcat(cwd, argv[1]);
 		}
-		if (dir = opendir(cwd))
+		if ((dir = opendir(cwd)))
 		{
 			closedir(dir);
 			current_dir = 0;
@@ -518,11 +522,22 @@ int main(int argc,char *argv[])
 			printf("Working in sever's directory\n");
 		}
 	}
+	// main loop for accepting connection and join new workers
 	while (1)
 	{
 		char *host = (char *)malloc(128);
 		char *service = (char *)malloc(128);
+		if (running_flag == 0)
+		{
+			printf("Exiting...\n");
+			break;
+		}
 		int cfd = accept_inet_stream_socket(sfd, host, 127, service, 127, LIBSOCKET_NUMERIC, 0);
+		if (running_flag == 0)
+		{
+			printf("Exiting...\n");
+			break;
+		}
 		if (cfd < 0)
 		{
 			free(host);
@@ -530,10 +545,11 @@ int main(int argc,char *argv[])
 			continue;
 		}
 		Client client = initClient(host, service, cfd, sfd);
-		//server(client);
-		thpool_add_work(thpool, (void *)server, client);
+		//server(client); //single thread testing
+		thpool_add_work(thpool, (void *)server, client); //using thread pool to handle connections
 	}
+	// destroying
 	thpool_destroy(thpool);
-	destroy_inet_socket(sfd);
+	//destroy_inet_socket(sfd);
 	return 0;
 }
